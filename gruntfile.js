15dcf0218e12ccc83b721402ff06cce1aafc3b55
Cardoz <jcardoz@deloitte.com>
2017-03-20 02:18:21 +0530

    initial project and grunt file setup for serving and building the website

---
+++ gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function(grunt) {
+
+    // 1. All configuration goes here 
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        concat: {
+            basic: {
+                src: ['src/scripts/custom.js'],
+                dest: '.tmp/scripts/custom.js',
+            },
+            extras: {
+                src: ['src/scripts/vendor/jquery-1.12.4.js', 'src/scripts/vendor/bootstrap.js'],
+                dest: '.tmp/scripts/vendor.js',
+            },
+        },
+        uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/scripts/custom.min.js': ['.tmp/scripts/custom.js'],
+                    'dist/scripts/vendor.min.js': ['.tmp/scripts/vendor.js'],
+                }
+            }
+        },
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/styles/images/',
+                    src: ['**/*.{png,jpg,gif}'],
+                    dest: 'dist/styles/images/'
+                }]
+            }
+        },
+        sass: {
+            dist: {
+                options: {
+                    sourcemap: 'none',
+                    style: 'compressed'
+                },
+                files: {
+                    '.tmp/styles/css/custom.css': 'src/styles/scss/custom.scss'
+                }
+            }
+        },
+        watch: {
+            options: {
+                livereload: true,
+            },
+            /*scripts: {
+                files: ['scripts/*.js'],
+                tasks: [],
+                options: {
+                    spawn: false,
+                },
+            },*/
+            css: {
+                files: ['styles/scss/*.scss'],
+                tasks: ['sass'],
+                options: {
+                    spawn: false,
+                }
+            }
+        },
+        cssmin: {
+            options: {
+                mergeIntoShorthands: false,
+                roundingPrecision: -1
+            },
+            target: {
+                files: {
+                    'dist/styles/vendor.min.css': ['src/styles/css/vendor/*.css'],
+                    'dist/styles/custom.min.css': ['.tmp/styles/css/custom.css']
+                }
+            }
+        },
+        copy: {
+            main: {
+                expand: true,
+                cwd: 'src',
+                src: ['**', '!**/scripts/vendor/**', '!**/scripts/build/**', '!**/scripts/custom.js', '!**/styles/scss/**', '!**/styles/css/**'],
+                dest: 'dist/',
+            },
+        }
+
+    });
+    // 3. Where we tell Grunt we plan to use this plug-in.
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    // 4. Where we tell Grunt what to do when we type "grunt" into the terminal.
+    grunt.registerTask('default', ['watch']);
+    grunt.registerTask('build', ['concat', 'uglify', 'sass', 'cssmin', 'imagemin', 'copy']);
+
+
+};
