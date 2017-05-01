37fb51aa1e726a191f0a8a19567abf2fa24022b9
Cardoz <jcardoz@deloitte.com>
2017-05-01 23:28:11 +0530

    update: added the our priests section to the landing page.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,13 @@
 $(document).ready(function() {
     // Initialize the datepicker
     $('.datepicker-container').datepicker();
+    $('.anyslider').anythingSlider({
+        resizeContents: true, // Resize contents to fit the container
+        buildNavigation: false, // No navigation
+        buildStartStop: false, // No start stop button
+        autoPlay: true, // will cycle automatically
+        startPanel: 1, // start at 1
+        pauseOnHover: true, // when the user hovers on a slide, pause the carousel
+        delay: 5000, // time to display each slide - 5 seconds per slide
+    });
 });
